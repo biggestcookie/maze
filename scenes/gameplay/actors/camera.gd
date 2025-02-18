35e3c88d2d4b28b12f9ea531716b9f0e8e22fc01
bigcookie <5748299+biggestcookie@users.noreply.github.com>
2025-02-18 12:21:20 -0500

    Fix ball movement damping

--- scenes/gameplay/actors/camera.gd
+++ scenes/gameplay/actors/camera.gd
@@ -4,14 +4,16 @@ extends Camera3D
 
 const follow_speed := 2
 
-func _physics_process(delta: float) -> void:
-	position.x = move_toward(
-		position.x,
-		follow_node.position.x,
-		delta * follow_speed
-	)
-	position.z = move_toward(
-		position.z,
-		follow_node.position.z,
-		delta * follow_speed
-	)
+func _physics_process(_delta: float) -> void:
+	position.x = follow_node.position.x
+	position.z = follow_node.position.z
+	# position.x = move_toward(
+	# 	position.x,
+	# 	follow_node.position.x,
+	# 	delta * follow_speed
+	# )
+	# position.z = move_toward(
+	# 	position.z,
+	# 	follow_node.position.z,
+	# 	delta * follow_speed
+	# )
--- scenes/gameplay/actors/player.gd
+++ scenes/gameplay/actors/player.gd
@@ -1,17 +1,21 @@
 extends RigidBody3D
 class_name Player
 
-const force_strength := 2
+const force_strength := 15
 
 var input := Vector2.ZERO
 
 func _process(_delta: float) -> void:
-	input.x = Input.get_accelerometer().x
-	input.y = Input.get_accelerometer().y * -1
+	if OS.get_name() == "Android":
+		input.x = Input.get_accelerometer().x * -1
+		input.y = Input.get_accelerometer().y
+	else:
+		input.x = Input.get_axis("move_right", "move_left")
+		input.y = Input.get_axis("move_down", "move_up")
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	var magnitude := input.normalized()
-	state.apply_force(
+	state.apply_central_force(
 		Vector3(
 			magnitude.x,
 			0,
