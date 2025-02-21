807939ff58809be9e7743480f2a53481e9a628f7
bigcookie <5748299+biggestcookie@users.noreply.github.com>
2025-02-21 08:06:12 -0500

    try clipping input

--- scenes/gameplay/actors/player.gd
+++ scenes/gameplay/actors/player.gd
@@ -9,16 +9,20 @@ func _process(_delta: float) -> void:
 	if OS.get_name() == "Android":
 		input.x = Input.get_gravity().x * -1
 		input.y = Input.get_gravity().y
+		input *= 0.2
 	else:
 		input.x = Input.get_axis("move_right", "move_left")
 		input.y = Input.get_axis("move_down", "move_up")
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	var magnitude := input.normalized()
+	var clipped := input.normalized()
+		if input.length() > 0.2
+		else Vector2.ZERO
+
 	state.apply_central_force(
 		Vector3(
-			magnitude.x,
+			clipped.x,
 			0,
-			magnitude.y
+			clipped.y
 		) * force_strength
 	)
