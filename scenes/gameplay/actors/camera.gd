8be6bd5cd2391fd7fc2e2a2d69956f4227b84efd
bigcookie <5748299+biggestcookie@users.noreply.github.com>
2025-02-27 10:11:36 -0500

    Android vertical camera follow

--- scenes/gameplay/actors/camera.gd
+++ scenes/gameplay/actors/camera.gd
@@ -3,17 +3,20 @@ extends Camera3D
 @export var follow_node: Node3D
 
 const follow_speed := 2
+const follow_height_offset := 9.5
 
-func _physics_process(_delta: float) -> void:
+func _ready() -> void:
+	position = Vector3(
+		follow_node.position.x,
+		follow_node.position.y + follow_height_offset,
+		follow_node.position.z,
+	)
+
+func _physics_process(delta: float) -> void:
 	position.x = follow_node.position.x
+	position.y = move_toward(
+		position.y,
+		follow_node.position.y + follow_height_offset,
+		delta * follow_speed
+	)
 	position.z = follow_node.position.z
-	# position.x = move_toward(
-	# 	position.x,
-	# 	follow_node.position.x,
-	# 	delta * follow_speed
-	# )
-	# position.z = move_toward(
-	# 	position.z,
-	# 	follow_node.position.z,
-	# 	delta * follow_speed
-	# )
--- scenes/gameplay/actors/player.gd
+++ scenes/gameplay/actors/player.gd
@@ -1,5 +1,5 @@
-extends RigidBody3D
 class_name Player
+extends RigidBody3D
 
 const force_strength := 15
 
@@ -9,20 +9,21 @@ func _process(_delta: float) -> void:
 	if OS.get_name() == "Android":
 		input.x = Input.get_gravity().x * -1
 		input.y = Input.get_gravity().y
-		input *= 0.2
 	else:
 		input.x = Input.get_axis("move_right", "move_left")
 		input.y = Input.get_axis("move_down", "move_up")
 
-func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	var clipped := input.normalized()
-		if input.length() > 0.2
-		else Vector2.ZERO
-
-	state.apply_central_force(
+func _physics_process(_delta: float) -> void:
+	var input_clipped := input
+	if OS.get_name() == "Android":
+		input_clipped = (input.normalized()
+			if input.length() > 0.2
+			else Vector2.ZERO
+		) * 0.2
+	apply_central_force(
 		Vector3(
-			clipped.x,
+			input_clipped.x,
 			0,
-			clipped.y
+			input_clipped.y
 		) * force_strength
 	)
