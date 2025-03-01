641ec2b8d812dcc68d1ef310d3fc28bf0a98d393
bigcookie <5748299+biggestcookie@users.noreply.github.com>
2025-03-01 10:25:49 -0500

    Replace sphere with capsule

--- scenes/gameplay/main.gd
+++ scenes/gameplay/main.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@onready var player: Player = $Player
 @onready var win_label: RichTextLabel = $UI/Win
 @onready var win_area: Area3D = $Level/Level3/Win/Area3D
 
@@ -8,6 +9,6 @@ func _ready() -> void:
 	win_area.body_entered.connect(on_win_body_entered)
 
 func on_win_body_entered(_body: Node3D) -> void:
-	get_tree().paused = true
+	player.paused = true
 	print("WIN!")
 	win_label.visible = true
